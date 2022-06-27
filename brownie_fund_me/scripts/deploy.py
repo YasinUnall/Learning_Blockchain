a8d64d0e5b71f7425e34fa1fefca2581cccf249f
YasinUnall <yasinunalnn@gmail.com>
2022-06-27 23:35:04 +0300

    Fund Me contract init. This contract can accept payments, Withdraw currency

---
+++ brownie_fund_me/scripts/deploy.py
@@ -0,0 +1,35 @@
+from brownie import FundMe, network, config, MockV3Aggregator
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
+from web3 import Web3
+
+
+def deploy_fund_me():
+    account = get_account()
+    # pass the price feed address to our FundMe contract
+
+    # if we are on a persistent network like rinkeby, use associated address
+    # otherwise, deploy mocks
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    print(f"Contract deployed to {fund_me.address}")
+    return fund_me
+
+
+def main():
+    deploy_fund_me()
+
